12eab09219fd01766e36fb6045239f3b27005599
Gaurav1185 <gda01185@gmail.com>
2022-12-11 23:38:22 +0530

    Assignment number 4 in operator

--- src/com/assignmentno3/operator/Qno4UsingOperators.java
+++ src/com/assignmentno3/operator/Qno4UsingOperators.java
@@ -1,7 +1,7 @@
 package com.assignmentno3.operator;
 //- Write a program to add 8 to the number 2345 and then divide it by 3.
 //- Now, the modulus of the quotient is taken with 5 and then multiply the resultant value by 5.
-//- Display the final result.
+
 public class Qno4UsingOperators {
     public static void main(String[] args){
         int GivenNumber=2345;
