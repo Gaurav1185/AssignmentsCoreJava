ce692cb8e4a71c86552b617d1f04b3a3b1fbd002
Gaurav1185 <gda01185@gmail.com>
2022-12-12 13:23:28 +0530

    Assignment number 5 in operator

---
+++ src/com/assignmentno3/operator/Qno7CheckCondition.java
@@ -0,0 +1,15 @@
+package com.assignmentno3.operator;
+//Assign values of variables 'a' and 'b' as 55 and 70 respectively and then check if both the conditions
+// 'a < 50' and 'a < b' are true.
+public class Qno7CheckCondition {
+    public static void main(String[] args) {
+        int a=55;
+        int b=70;
+        if(a<50){
+            System.out.println("A is less than 50 "+a);
+        }
+        else if(a<b){
+            System.out.println("A is less than B");
+        }
+    }
+}
