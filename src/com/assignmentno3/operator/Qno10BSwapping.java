7eab72dfc3051d6564b3570a1671007cd267644d
Gaurav1185 <gda01185@gmail.com>
2022-12-15 23:11:30 +0530

    Swapping without using variable

---
+++ src/com/assignmentno3/operator/Qno10BSwapping.java
@@ -0,0 +1,17 @@
+package com.assignmentno3.operator;
+
+import java.util.Scanner;
+
+public class Qno10BSwapping {
+    public static void main(String[] args) {
+        Scanner values=new Scanner(System.in);
+        System.out.println("Enter first number");
+        int value1=values.nextInt();
+        System.out.println("Enter second number");
+        int value2=values.nextInt();
+        value1=value1+value2;
+        value2=value1-value2;
+        value1=value1-value2;
+        System.out.println(value1+""+value2);
+    }
+}
