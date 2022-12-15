0090c4465c0ac0cf7d177a3f4e538b93b88fd234
Gaurav1185 <gda01185@gmail.com>
2022-12-15 21:30:53 +0530

    Assignment number 5 in operator

---
+++ src/com/assignmentno3/operator/Qno8LogicalOperator.java
@@ -0,0 +1,11 @@
+package com.assignmentno3.operator;
+//Now solve the above question[assignment - 07] to check if at-least one of the conditions
+// 'a < 50' or 'a < b' is true.
+public class Qno8LogicalOperator {
+    public static void main(String[] args) {
+         char a=55;
+         char b=70;
+        System.out.println();
+        System.out.println();
+    }
+}
